44e79737b36ccf887d4476af9c345d51a4f8d5ea
Polina <75391420+wezbicka@users.noreply.github.com>
2022-08-06 13:57:51 +0300

    Publish photo to Telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -9,4 +9,6 @@ if __name__ == "__main__":
     load_dotenv()
     tg_token = os.environ['TG_TOKEN']
     bot = telegram.Bot(token=tg_token)
-    bot.send_message(text='The Space is well!', chat_id="@SpaceWezhbicka")
+    chat_id = "@SpaceWezhbicka"
+    # bot.send_message(text='The Space is well!', chat_id=chat_id)
+    bot.send_document(chat_id=chat_id, document=open('images/nasa_apod_0.jpg', 'rb'))
