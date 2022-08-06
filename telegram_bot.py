147c287b3c52fc457bd3b87a815a7ef013e3891a
Polina <75391420+wezbicka@users.noreply.github.com>
2022-08-06 13:47:01 +0300

    Publish text to Telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     load_dotenv()
     tg_token = os.environ['TG_TOKEN']
     bot = telegram.Bot(token=tg_token)
-    print(bot.get_me())
+    bot.send_message(text='The Space is well!', chat_id="@SpaceWezhbicka")
