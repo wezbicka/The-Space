569027d0214c01e61e208cb3dc99232dd86f4605
Polina <75391420+wezbicka@users.noreply.github.com>
2022-08-08 14:18:50 +0300

    Add script launch in fetch_spacex_images.py

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -14,8 +14,7 @@ def get_id():
     return args.id
 
 
-def fetch_spacex_last_launch():
-    # id = "5eb87d47ffd86e000604b38a"
+def fetch_spacex_launch():
     id = get_id()
     image_url = f"https://api.spacexdata.com/v5/launches/{id}"
     response = requests.get(image_url)
@@ -24,3 +23,11 @@ def fetch_spacex_last_launch():
     for num, link in enumerate(images_links):
         filename = f'spacex_{num}.jpeg'
         image_config.py.download_image(link, f"{image_config.py.DIRECTORY}/{filename}")
+
+
+if __name__ == '__main__':
+    image_config.create_directory()
+    parser = argparse.ArgumentParser(description='Программа скачивает фотографии с запуска ракет от компании SpaceX')
+    parser.add_argument('-id', '--id', help='ID запуска')
+    args = parser.parse_args()
+    fetch_spacex_launch(args.id)
