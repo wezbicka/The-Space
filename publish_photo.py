3b48cd5eef71873c971d8947ba17e7367adfb89a
Polina <75391420+wezbicka@users.noreply.github.com>
2022-08-15 15:46:17 +0300

    Update publish_photo.py

--- publish_photo.py
+++ publish_photo.py
@@ -8,28 +8,26 @@ from dotenv import load_dotenv
 import image_config
 
 
-TG_TOKEN = os.environ['TG_TOKEN']
-CHAT_ID = os.environ["TG_CHAT_ID"]
-bot = telegram.Bot(token=TG_TOKEN)
-
-
-def send_photo(file_name):
+def send_photo(file_name, chat_id):
     if file_name == '':
         image = random.choice(image_config.DIRECTORY)
         with open(os.path.join(image_config.DIRECTORY, image), 'rb') as file:
             bot.send_document(
-                chat_id=CHAT_ID,
+                chat_id=chat_id,
                 document=file
             )
     else:
         bot.send_document(
-            chat_id=CHAT_ID,
+            chat_id=chat_id,
             document=open(os.path.join(image_config.DIRECTORY, file_name), 'rb')
         )
 
 
 if __name__ == '__main__':
     load_dotenv()
+    tg_token = os.environ['TG_TOKEN']
+    bot = telegram.Bot(token=tg_token)
+    chat_id = os.environ["TG_CHAT_ID"]
     parser = argparse.ArgumentParser(description='Программа отправляет фотографию из папки images')
     parser.add_argument(
         '-n',
@@ -38,4 +36,4 @@ if __name__ == '__main__':
         default=''
     )
     args = parser.parse_args()
-    send_photo(args.file_name)
+    send_photo(args.file_name, chat_id)
