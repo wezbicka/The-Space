b104d0f481bd0a27955068316d4691d8b4c04041
Polina <75391420+wezbicka@users.noreply.github.com>
2022-08-19 10:48:03 +0300

    Add functions for send_image & select_photo

--- publish_photo.py
+++ publish_photo.py
@@ -8,21 +8,21 @@ from dotenv import load_dotenv
 import image_config
 
 
-def send_photo(file_name, chat_id):
+def send_image(image, chat_id):
+    with open(os.path.join(image_config.DIRECTORY, image), 'rb') as file:
+            bot.send_document(
+                chat_id=chat_id,
+                document=file
+            )
+
+
+def select_photo(file_name):
     images = os.listdir(image_config.DIRECTORY)
     if file_name == '':
         image = random.choice(images)
-        with open(os.path.join(image_config.DIRECTORY, image), 'rb') as file:
-            bot.send_document(
-                chat_id=chat_id,
-                document=file
-            )
+        return image
     else:
-        with open(os.path.join(image_config.DIRECTORY, file_name), 'rb') as file:
-            bot.send_document(
-                chat_id=chat_id,
-                document=file
-            )
+        return file_name
 
 
 if __name__ == '__main__':
@@ -38,4 +38,5 @@ if __name__ == '__main__':
         default=''
     )
     args = parser.parse_args()
-    send_photo(args.file_name, chat_id)
+    image = select_photo(args.file_name)
+    send_image(image, chat_id)
